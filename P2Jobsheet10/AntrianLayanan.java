ec016364d72a2da1b1ef62cf488b9ee18c2b3e19
DhestaLingga06 <dhestalingga21@gmail.com>
2026-04-29 09:55:54 +0700

    Modif

--- P2Jobsheet10/AntrianLayanan.java
+++ P2Jobsheet10/AntrianLayanan.java
@@ -81,4 +81,13 @@ public class AntrianLayanan {
         size--;
         return mhs;   
     }
+    public void lihatAkhir() {
+        if (IsEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+        System.out.println("Mahasiswa paling belakang: ");
+        System.out.println("NIM - NAMA - PRODI - KELAS");
+        data[rear].tampilkanData();
+        }
+    }
 }
