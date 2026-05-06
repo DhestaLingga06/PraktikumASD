c543148d26c3d8b66b84c5984cf171733c0acfa5
DhestaLingga06 <dhestalingga21@gmail.com>
2026-05-06 10:28:42 +0700

    Percobaan 2

--- Pertemuan12/SLLMain06.java
+++ Pertemuan12/SLLMain06.java
@@ -1,20 +1,25 @@
-import java.util.Scanner;
 public class SLLMain06 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         SingleLinkedList06 sll = new SingleLinkedList06();
 
-        System.out.print("Masukkan NIM : ");
-        String nim = sc.nextLine();
-        System.out.print("Masukkan Nama : ");
-        String nama = sc.nextLine();
-        System.out.print("Masukkan Kelas : ");
-        String kelas = sc.nextLine();
-        System.out.print("Masukkan IPK : ");
-        double ipk = sc.nextDouble();
+        Mahasiswa06 mhs1 = new Mahasiswa06("24212200", "Alvaro", "1A", 4.0);
+        Mahasiswa06 mhs2 = new Mahasiswa06("23212201", "Bimon", "2B", 3.8);
+        Mahasiswa06 mhs3 = new Mahasiswa06("22212202", "Cintia", "3C", 3.5);
+        Mahasiswa06 mhs4 = new Mahasiswa06("21212203", "Dirga", "4D", 3.6);
 
-        Mahasiswa06 mhs = new Mahasiswa06(nim,nama,kelas,ipk);
-        sll.addLast(mhs);
+        sll.addFirst(mhs4);
+        sll.addLast(mhs3);
+        sll.addLast(mhs2);
+        sll.addLast(mhs1);
+
+        System.out.println("data index 1 : ");
+        sll.getData(1);
+        System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexOf("bimon"));
+        System.out.println();
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
         sll.print();
     }
 }
