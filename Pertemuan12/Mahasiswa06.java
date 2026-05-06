30ee0ab9de35b7325295c1ba2acd3051790fdd51
DhestaLingga06 <dhestalingga21@gmail.com>
2026-05-06 09:25:27 +0700

    Percobaan 1

---
+++ Pertemuan12/Mahasiswa06.java
@@ -0,0 +1,19 @@
+public class Mahasiswa06 {
+
+    String nim;
+    String nama;
+    String kelas;
+    double ipk;
+
+    Mahasiswa06() {
+    }
+    public Mahasiswa06(String nm, String name, String kls, double ip) {
+        nim = nm;
+        nama = name;
+        kelas = kls;
+        ipk = ip;
+    }
+    public void tampilInformasi() {
+        System.out.printf("%-10s %-10s %-5s %-1f\n", nama, nim, kelas, ipk);
+    }
+}
