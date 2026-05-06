4aad24ca467491f3e90419ea727ff4ff3b54ac69
DhestaLingga06 <dhestalingga21@gmail.com>
2026-05-06 09:41:04 +0700

    Modif

--- Pertemuan12/SLLMain06.java
+++ Pertemuan12/SLLMain06.java
@@ -1,19 +1,20 @@
+import java.util.Scanner;
 public class SLLMain06 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         SingleLinkedList06 sll = new SingleLinkedList06();
 
-        Mahasiswa06 mhs1 = new Mahasiswa06("24212200", "Alvaro", "1A", 4.0);
-        Mahasiswa06 mhs2 = new Mahasiswa06("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa06 mhs3 = new Mahasiswa06("22212202", "Cintia", "3C", 3.5);
-        Mahasiswa06 mhs4 = new Mahasiswa06("21212203", "Dirga", "4D", 3.6);
+        System.out.print("Masukkan NIM : ");
+        String nim = sc.nextLine();
+        System.out.print("Masukkan Nama : ");
+        String nama = sc.nextLine();
+        System.out.print("Masukkan Kelas : ");
+        String kelas = sc.nextLine();
+        System.out.print("Masukkan IPK : ");
+        double ipk = sc.nextDouble();
 
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
+        Mahasiswa06 mhs = new Mahasiswa06(nim,nama,kelas,ipk);
+        sll.addLast(mhs);
         sll.print();
     }
 }
