9e7b57cf5d6f749b0ba179f959774f1b63987c1b
DhestaLingga06 <dhestalingga21@gmail.com>
2026-05-18 22:11:41 +0700

    CM2

---
+++ JOBSHEET CM2/DoubleLinkedList06.java
@@ -0,0 +1,118 @@
+public class DoubleLinkedList06 {
+    
+    Node06 head;
+    Node06 tail;
+    Node06 headPesanan;
+    Node06 tailPesanan;
+    int noAntrian = 1;
+
+    public DoubleLinkedList06() {
+        head = null;
+        tail = null;
+    }
+    public boolean isEmpty() {
+        return head == null;
+    }
+    public void tambahLaporan(Pesanan06 pesanan) {
+        Node06 newNode = new Node06(null, 0, null, pesanan, null);
+        if (headPesanan == null) {
+            headPesanan = tailPesanan = newNode;
+        } else {
+            tailPesanan.next = newNode;
+            newNode.prev = tailPesanan;
+            tailPesanan = newNode;
+        }
+    }
+    //Tambah Antrian
+    public void tambahAntrian(Pembeli06 pembeli) {
+        Node06 newNode = new Node06(null, noAntrian++, pembeli, null, null);
+        if (isEmpty()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+        System.out.println("Antrian berhasil ditambahkan dengan nomor: " + newNode.noAntrian);
+    }
+    //Cetak Antrian
+    public void cetakAntrian() {
+        if (isEmpty()) {
+            System.out.println("Antrian masih kosong");
+            return;
+        }
+
+        Node06 current = head;
+        System.out.println("=============================");
+        System.out.println("DAFTAR ANTRIAN PEMBELI");
+        System.out.println("=============================");
+        System.out.printf("%-12s %-15s %-15s\n", "No Antrian ", "Nama ", "No HP ");
+        while (current != null) {
+            System.out.printf("%-12d %-15s %-15s\n", current.noAntrian, 
+            current.pembeli.namaPembeli, current.pembeli.noHp);
+            current = current.next;
+        }
+    }
+    //Hapus Antrian dan Pesan
+    public void removeAntrian(Pesanan06 pesanan) {
+        if (isEmpty()) {
+            System.out.println("Antrian masih kosong.");
+            return;
+        }
+        Node06 current = head;
+        System.out.println("No.Antrian " + current.noAntrian + " - " + current.pembeli.namaPembeli + " telah memesan " + pesanan.namaPesanan);
+        tambahLaporan(pesanan);
+        head = head.next;
+        if (head != null) {
+            head.prev = null;
+        } else {
+            tail = null;
+        }
+    }
+    //Sorting Pesanan
+    public void sortingPesanan() {
+        if (headPesanan == null) return;
+        boolean berubah;
+        do {
+            berubah = false;
+            Node06 current = headPesanan;
+            while (current.next != null) {
+                if (current.pesanan.namaPesanan.compareToIgnoreCase(
+                    current.next.pesanan.namaPesanan) > 0) {
+                    Pesanan06 temp = current.pesanan;
+                    current.pesanan = current.next.pesanan;
+                    current.next.pesanan = temp;
+                    berubah = true;
+                }
+                current = current.next;
+            }
+        } while (berubah);
+    }
+    //Laporan Pesanan
+    public void laporanPesanan() {
+        
+        sortingPesanan();
+        Node06 current = headPesanan;
+        boolean addData = false;
+        int totalPendapatan = 0;
+        System.out.println("======================================");
+        System.out.println("LAPORAN PESANAN (URUT NAMA PESANAN)");
+        System.out.println("======================================");
+        System.out.printf("%-15s %-18s %-10s\n","Kode Pesanan","Nama Pesanan","Harga");
+        while (current != null) {
+            if (current.pesanan != null) {
+                addData = true;
+                System.out.printf("%-15d %-18s %-10d\n",current.pesanan.kodePesanan,
+                current.pesanan.namaPesanan,current.pesanan.harga);
+                totalPendapatan += current.pesanan.harga;
+            }
+            current = current.next;
+        }
+        if (!addData) {
+            System.out.println("Belum ada pesanan");
+        } else {
+            System.out.println("================================");
+            System.out.println("Total Pendapatan : Rp " + totalPendapatan);
+        }
+    }
+}
---
+++ JOBSHEET CM2/DoubleLinkedListMain06.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class DoubleLinkedListMain06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DoubleLinkedList06 list = new DoubleLinkedList06();
+        int pilih;
+
+        do {
+            System.out.println("================================");
+            System.out.println("SISTEM ANTRIAN ROYAL DELISH");
+            System.out.println("================================");
+            System.out.println("1. Tambah Antrian");
+            System.out.println("2. Cetak Antrian");
+            System.out.println("3. Hapus Antrian dan Pesan");
+            System.out.println("4. Laporan Pesanan");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu : ");
+            pilih = sc.nextInt();
+            sc.nextLine();
+
+            switch (pilih) {
+                case 1:
+                    System.out.print("Nama Pembeli : ");
+                    String nama = sc.nextLine();
+                    System.out.print("No HP   : ");
+                    String noHp = sc.nextLine();
+                    Pembeli06 pembeli = new Pembeli06(nama, noHp);
+                    list.tambahAntrian(pembeli);
+                    break;
+            
+                case 2:
+                    list.cetakAntrian();
+                    break;
+                
+                case 3:
+                    System.out.print("Kode Pesanan : ");
+                    int kodePesanan = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Nama Pesanan : ");
+                    String namaPesanan = sc.nextLine();
+                    System.out.print("Harga        : ");
+                    int harga = sc.nextInt();
+                    sc.nextLine();
+                    Pesanan06 pesanan= new Pesanan06(kodePesanan, namaPesanan, harga);
+                    list.removeAntrian(pesanan);
+                    break;
+                
+                case 4:
+                    list.laporanPesanan();
+                    break;
+
+                case 0:
+                    System.out.println("Program Selesai");
+                    break;
+                default:
+                    System.out.println("Menu tidak valid");
+            }
+        } while (pilih != 0);
+    }
+}
